09a1e1144220438ea768332beecf2256b56407ab
RMDBIG <Kayodenoah08@gmail.com>
2024-08-31 22:01:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log(40+70-20-10+15);
+      let firstName="kayode";
+      console.log(firstName); 
+      console.log(firstName); 
+      console.log(firstName);
+
+
+      
+      let PI=3.142
+      console.log(PI);
+      console.log(typeof PI)
+
+      let myfirstName="joan"
+      let myfirstJob="programmer"
+      let mycurrentJob="forex trader"
+      console.log(myfirstName);
+      console.log(myfirstJob);
+      console.log(mycurrentJob);
+ 
+      const country="Nigeria";
+      const continent="Africa";
+      let population=500000000;
+      console.log(country);
+      console.log(continent);
+      console.log(population);
+     let longman = "true";
+     const isisland = "false";
+     const age = true;
+     const maltilda = 23;
+     console.log(typeof "longman");
+     console.log(typeof "isisland");
+     console.log(typeof age);
+     console.log(typeof maltilda);
