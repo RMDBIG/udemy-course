1c9d4ea46b6e789fee47610a0f3105694c245d22
RMDBIG <Kayodenoah08@gmail.com>
2024-09-08 17:43:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,207 +1,54 @@
-let js ='amazing';
-      /*console.log(40+70-20-10+15);
-      let firstName="kayode";
-      console.log(firstName); 
-      console.log(firstName); 
-      console.log(firstName);
-
-
-      
-      let PI=3.142
-      console.log(PI);
-      console.log(typeof PI)
-
-      let myfirstName="joan"
-      let myfirstJob="programmer"
-      let mycurrentJob="forex trader"
-      console.log(myfirstName);
-      console.log(myfirstJob);
-      console.log(mycurrentJob);
- 
-      const country="Nigeria";
-      const continent="Africa";
-      let population=500000000;
-      console.log(country);
-      console.log(continent);
-      console.log(population);
-
-      const isIsland = false;
-      //isIsland=true;
-      let language = "yoruba"
-     
-      console.log(typeof isIsland);
-      console.log(typeof population);
-      console.log(typeof "Country");
-      console.log(typeof "language");
-      const now = 2045
-      const ageTemitope = now-2024
-      const ageKayode = now-2024
-      console.log(ageTemitope);
-      console.log(ageKayode);
-      console.log(ageTemitope*2);
-      console.log(ageKayode/3);
-      console.log(2**3);
-      This means 2 to the power of 3 that is 2 raised to power 3
-      that is 2*2*2=8
-      const firstname = "Kayode";
-      const lastname = "Temitope";
-      console.log(firstname + ' ' + lastname);
-      
-      let x = 10+5; //15
-      x +=10; // this means x+10
-      x *=4; //this means x multiply by 4 which is equal to 100
-      x++;
-      x--;
-      x--;
-      console.log(x);
-      console.log(ageTemitope > ageKayode);*/
-
-
-      /* Mark and John are trying to compare their BMI (Body Mass Index), which is
-calculated using the formula:
-BMI = mass / height ** 2 = mass / (height * height) (mass in kg
-and height in meter).
-Your tasks:
-1. Store Mark's and John's mass and height in variables
-2. Calculate both their BMIs using the formula (you can even implement both
-versions)
-3. Create a Boolean variable 'markHigherBMI' containing information about
-whether Mark has a higher BMI than John.
-Test data:
-§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
-m tall.
-§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
-m tall.
-GOOD LUCK */
-
-/*
-const massMark = 78;
-const massJohn = 92;
-const heightMark = 1.69;
-const heightJohn = 1.95;
-
-
-const BMIMark = massMark/ heightMark ** 2;
-const BMIJohn = massJohn/ (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn);
-const markHigherBMI = BMIMark > BMIJohn;
-console.log(markHigherBMI);
-
-//const massMark = 95
-const massJohn = 85
-const heightMark = 1.88
-const heightJohn = 1.76
-
-const BMIMark = massMark/ heightMark ** 2;
-const BMIJohn = massJohn/ (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn);
-const markHigherBMI = BMIMark > BMIJohn;
-console.log(markHigherBMI);
-
-
-
-const firstName = "Jonas";
-const birthYear = 2000;
-const year = 2037;
-const job ="producer";
-
-const jonas = "I'm " + firstName + ", a " + (year - birthYear ) + " years old " + job + "! ";
-console.log(jonas);
-
-const jonasNew = `I'm ${firstName}`;
-console.log(jonasNew);
-console.log(`just a regular string...`);
-console.log(`string with \n\
-      mutiples \n\
-      lines`);
-
-      const firstName = "Kayode";
-      const business = "Forex trader" ;
-      const institution = "OAU";
-
-      const Kayode = `I'm ${firstName} a ${business} attending ${institution}`;
-
-      console.log(Kayode); 
-
-
-const age = 15
-
-if  (age>= 18) {
-      console.log(`sarah can start driving license car`);
-} else {
-      const yearsLeft = 18 - age ;
-      console.log(`sarah is too young wait another ${yearsLeft} years`);
-} 
-
-const population = 130
-const country = "Portugal"
-if (population > 33) {
-      console.log(`${country}'s population is above average`);
-    } else {
-      console.log(
-        `${country}'s population is ${33 - population} million
-        below average`
-      );
-    } 
-
-     const massMark = 78;
-      const massJohn = 92;
-const heightMark = 1.69;
-const heightJohn = 1.95;
-
-
-const BMIMark = massMark/ heightMark ** 2;
-const BMIJohn = massJohn/ (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn);
-
-const massMark = 95
-const massJohn = 85
-const heightMark = 1.88
-const heightJohn = 1.76
-
-const BMIMark = massMark/ heightMark ** 2;
-const BMIJohn = massJohn/ (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn);
-if(BMIMark > BMIJohn) {
-      console.log("Mark's BMI is higher than John's!")
-}   else {
-            console.log("John's BMI is higher than Mark's!"
-            );
-      }
-     const myYear = '2000';
-     console.log(Number(myYear));
-     console.log(myYear + 20);
-     
-
-const inputYear = '1991';
-console.log(Number(inputYear), inputYear);
-console.log(inputYear + 18);
-
-
-
-const money = 0
-if(money) {
-      console.log(`you should spend it`)
-} else{
-      console.log(`you should hustle`)
+const day = 'Monday'
+switch (day) {
+    case 'Monday':
+        console.log('plan goes to structure')
+        console.log('Go to coding meeting')
+        break;
+        case 'Tuesday':
+            case 'Wednesday':
+                console.log('They should resume quickly')
+                console.log('What are they still doing at home')
+                break;
+                case 'Thursday':
+                    case 'Friday':
+                        console.log('school closes by then')
+                        console.log('yeah that is it')
+                        break;
+                        default:
+                            console.log('not a valid day')
 }
-
-
-
-
-const age = '18';
-if(18===18){
-      console.log(`let it be defined : D (strictly)`)
-} else {
-      console.log(`let it be undefined :D (loose)`);
-}
- */
-const age = '18';
-if(age===18){
-      console.log(`let it be defined : D (strictly)`)
-} 
-if(age==18 ) {
-      console.log(`let it be undefined :D (loose)`);
-}
-
-const year = 1991
+const language = 'Spanish'
+switch(language) {
+    case 'Chinese':
+    case 'Madarin':
+        console.log('Most number of speakers!')
+        break;
+        case 'Spanish':
+            console.log('Second place in number of native speakers')
+            break;
+            case 'english':
+              console.log('3rd place');
+              break;
+            case 'hindi':
+              console.log('Number 4');
+              break;
+            case 'arabic':
+              console.log('5th most spoken language');
+              break;
+            default:
+              console.log('Great language too :D');
+          }
+const population='34000000';
+const country='Portugal';
+console.log(
+    `${country}'s population is ${population>33000000? 'below':'above'} average`
+)
+/*const bill=275;
+const tip= bill <=300 && bill >=50 ? bill*0.15:bill*0.2;
+console.log(`the bill value was ${bill}, and the tip value was ${tip}, and the total value is ${bill+tip}`);
+const bill=40;
+const tip= bill <=300 && bill >=50 ? bill*0.15:bill*0.2;
+console.log(`the bill value was ${bill}, and the tip value was ${tip}, and the total value is ${bill+tip}`);*/
+const bill=430;
+const tip= bill <=300 && bill >=50 ? bill*0.15:bill*0.2;
+console.log(`the bill value was ${bill}, and the tip value was ${tip}, and the total value is ${bill+tip}`);
